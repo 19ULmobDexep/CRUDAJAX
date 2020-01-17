e338f763b7e971bb4bb4f10ea39511670f10eaa7
19ULmobDexep <s.zai@it-students.fr>
2020-01-17 16:24:20 +0100

    first commit

---
+++ evaluation2.js
@@ -0,0 +1,165 @@
+/*/================================================================================================================================================
+===================================================================================================================================================
+====================================================            Les  Musiques               ======================================================
+================================================================================================================================================/*/
+
+
+
+
+/*/======================== Afficher les musiques de l'API =======================================/*/
+
+function rafraichir() {
+    $('#Ibody').empty();
+    $.get({
+        url: 'http://jihane.fr/dwmg2/api/music/liste.php',
+        success: function(musiques) {
+            console.log(musiques)
+
+            for (i = 0; i < musiques.length; i++) {
+                console.log(musiques[i].Titres)
+
+                $('#Ibody').append('<tr><td>' + musiques[i].id + '</td><td>' + musiques[i].Titres + '</td><td>' +
+                    musiques[i].Artistes + '</td><td>' + '<button id="bouttone" class="btn btn-warning" onclick="reveal(' + musiques[i].id + ')"><i class="far fa-eye"></i></button>' + '</td></tr>')
+            }
+        },
+        dataType: "json",
+    })
+}
+rafraichir()
+
+
+
+
+/*/======================== Afficher le 1er Modal ================================================/*/
+
+$("#myBtn").click(function() {
+    $("#myModal").modal('show');
+})
+
+
+/*/================== Créer une nouvelle Musique, la fermer et réactualiser la page ==============/*/
+
+
+function ajout() {
+
+    $.post({
+        url: 'http://jihane.fr/dwmg2/api/music/create.php',
+
+        data: {
+            Titres: $("#titre").val(),
+            Artistes: $('#artiste').val(),
+            Genres: $('#genre').val(),
+            Dates: $('#date').val(),
+            Temps: $('#temps').val(),
+        },
+        success: function(musiques) {
+            console.log(musiques);
+            rafraichir()
+        },
+        dataType: "text"
+    })
+
+}
+$("#myBtn2").click(function() {
+    $("#myModal").modal('hide')
+
+})
+
+
+/*/=================== Afficher le 2ème Modal et les données =============/*/
+
+
+var mid;
+
+function reveal(idv) {
+
+    mid = idv;
+
+    $("#myModal2").modal('show');
+
+    $.get({
+        url: 'http://jihane.fr/dwmg2/api/music/read.php',
+
+        data: {
+            id: idv
+        },
+        success: function(musique) {
+            $("#titre2").val(musique.Titres)
+            $("#artiste2").val(musique.Artistes)
+            $("#genre2").val(musique.Genres)
+            $("#date2").val(musique.Dates)
+            $("#temps2").val(musique.Temps)
+        },
+        dataType: "json"
+    })
+    rafraichir()
+}
+
+/*/ ================== Fonction Delete et Modifier =============/*/
+
+
+
+function supprimer() {
+
+    $.get({
+
+        url: 'http://jihane.fr/dwmg2/api/music/delete.php',
+
+        data: {
+            id: mid
+
+        },
+        success: function(musique) {
+            $("#titre2").val(musique.Titres)
+            $("#artiste2").val(musique.Artistes)
+            $("#genre2").val(musique.Genres)
+            $("#date2").val(musique.Dates)
+            $("#temps2").val(musique.Temps)
+            rafraichir()
+        },
+        dataType: "text"
+    })
+
+}
+$("#supress").click(function() {
+    $("#myModal2").modal('hide');
+
+})
+
+
+
+function modifier() {
+
+    $.get({
+
+        url: 'http://jihane.fr/dwmg2/api/music/update.php',
+
+        data: {
+            id: mid,
+
+            Titres: $("#titre2").val(),
+            Artistes: $("#artiste2").val(),
+            Genres: $("#genre2").val(),
+            Dates: $("#date2").val(),
+            Temps: $("#temps2").val(),
+
+        },
+        success: function(musiques) {
+            console.log(musiques);
+            rafraichir()
+        },
+        dataType: "text"
+    })
+
+}
+$("#modify").click(function() {
+    $("#myModal2").modal('hide');
+
+})
+
+
+
+/*/================================================================================================================================================
+===================================================================================================================================================
+====================================================            Les  voitures               ======================================================
+================================================================================================================================================/*/
---
+++ evaluationjs3.js
@@ -0,0 +1,103 @@
+/*/ ===================== Afficher la liste de voitures =======================
+============================================================================/*/
+
+
+fetch('http://jihane.fr/dwmg2/api/voiture/liste.php')
+
+.then(
+    function(response) {
+        return response.json()
+    }
+)
+
+.then(
+    function(voitures) {
+
+
+
+
+        for (i = 0; i < voitures.length; i++) {
+            console.log(voitures[i]);
+
+
+            var tr = document.createElement('tr');
+            var td = document.createElement('td');
+
+            tr.innerHTML += "<td>" + voitures[i].id + "</td>" + "<td>" + voitures[i].marque + "</td>" + "<td>" + voitures[i].model + "</td>" + "<td>" + voitures[i].gamme + "</td>" + "<td>" + voitures[i].prix + "</td><td>" + '<button class="btn btn-warning" onclick="reveal(' + voitures[i].id + ')"><i class="far fa-eye"></i></button>' + "</td></tr> ";
+
+            var parent = document.getElementById('Ibody')
+            parent.appendChild(tr);
+
+        }
+
+    }
+)
+
+
+
+/*/======================= Afficher 1er Modal et 2ème Modal ==================================/*/
+
+
+$("#myBtn").click(function() {
+    $("#myModal").modal('show');
+})
+
+
+
+
+function reveal(idv) {
+
+
+
+    $("#myModal2").modal('show');
+
+    $.get({
+        url: 'http://jihane.fr/dwmg2/api/voiture/liste.php',
+
+        data: {
+            id: idv
+        },
+        success: function(voiture) {
+            $("#marque").val(voiture.marque)
+            $("#modele2").val(voiture.model)
+            $("#gamme2").val(voiture.gamme)
+            $("#prix2").val(voiture.prix)
+
+        },
+        dataType: "json"
+    })
+
+}
+
+
+
+/*/============================= Créer une nouvelle voiture ==================================/*/
+
+
+function ajout() {
+
+    fetch('http://jihane.fr/dwmg2/api/voiture/create.php', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-type': 'application/json',
+            }
+
+        })
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(voitures) {
+            console.log(voitures[i])
+            marque = document.getElementById("#marque").value()
+            model = document.getElementById('#modele').value()
+            gamme = document.getElementById('#gamme').value()
+            prix = document.getElementById('#prix').value()
+
+        });
+
+}
+
+$("#myBtn2").click(function() {
+    $("#myModal").modal('hide');
+})
